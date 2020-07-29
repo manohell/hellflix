3b09bb02ade0b5c7e5c4587c4968034b25421621
unknown <heitor@excellenceinit.com>
2020-07-29 20:06:15 -0300

    publica2

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Home from './Home';
-import { BrowserRouter, Switch } from 'react-router-dom'
+import { BrowserRouter, Switch, React } from 'react-router-dom'
 
 ReactDOM.render(
   <BrowserRouter>
