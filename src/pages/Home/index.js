e3433fcb1dea67c3f432181ae369854b3e36930d
unknown <heitor@excellenceinit.com>
2020-08-10 19:16:58 -0300

    publica7

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -40,7 +40,6 @@ function Home() {
             </div>
           );
         }
-
         return (
           <Carousel
             key={categoria.id}
@@ -48,28 +47,6 @@ function Home() {
           />
         );
       })}
-
-      {/* <BannerMain
-        videoTitle={dadosIniciais.categorias[0].videos[0].titulo}
-        url={dadosIniciais.categorias[0].videos[0].url}
-        videoDescription="O que"
-      />
-      <Carousel
-        ignoreFirstVideo
-        category={dadosIniciais.categorias[0]}
-      />
-      <Carousel
-        category={dadosIniciais.categorias[1]}
-      />
-      <Carousel
-        category={dadosIniciais.categorias[2]}
-      />
-      <Carousel
-        category={dadosIniciais.categorias[3]}
-      />
-      <Carousel
-        category={dadosIniciais.categorias[4]}
-      /> */}
     </PageDefault>
   );
 }
